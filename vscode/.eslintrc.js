20fc9abc337e23e55fc91c614801d0485db488f5
kevinhsu <kevinhsu@synology.com>
2018-09-08 16:33:28 +0800

    Add package.json and .eslintrc.js

---
+++ vscode/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'standard',
+    'plugin:vue/recommended',
+  ],
+  globals: {
+    __static: true
+  },
+  plugins: [
+    'vue',
+    'prettier'
+  ],
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  },
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  }
+}
